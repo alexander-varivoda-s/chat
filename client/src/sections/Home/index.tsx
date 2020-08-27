6430637d982beb61cdf2dfa17b7d5f61bd5c8919
Alexander Varivoda <alexander.varivoda.s@gmail.com>
2020-08-27 11:31:00 +0300

    Basic direct messages list implementation with styling

--- client/src/sections/Home/index.tsx
+++ client/src/sections/Home/index.tsx
@@ -1,5 +1,46 @@
+import { MessageOutlined, PlusOutlined } from '@ant-design/icons';
+import { Button, Layout, List, Typography } from 'antd';
 import React from 'react';
+import './styles/index.scss';
+
+const { Sider, Content } = Layout;
+const { Text } = Typography;
 
 export const Home = () => {
-  return (<h2>Home</h2>)
+  const directMessagesHeaderElement = (
+    <Button
+      size="small"
+      className="direct-messages__btn"
+      type="link"
+      icon={<MessageOutlined />}
+    >
+      <Text>Direct Messages (2)</Text>
+      <PlusOutlined />
+    </Button>
+  );
+
+  return (
+    <Layout className="home">
+      <Sider className="home__sider" width={250}>
+        <List
+          header={directMessagesHeaderElement}
+          className="direct-messages"
+        >
+          <List.Item>
+            <Button type="link" size="small" className="direct-messages__btn">
+              <Text>Alexander</Text>
+            </Button>
+            <span className="online-indicator online-indicator--active" />
+          </List.Item>
+          <List.Item>
+            <Button type="link" size="small" className="direct-messages__btn">
+              <Text>User</Text>
+            </Button>
+            <span className="online-indicator" />
+          </List.Item>
+        </List>
+      </Sider>
+      <Content className="home__content">Home Content</Content>
+    </Layout>
+  );
 };
